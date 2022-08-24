1288e544678de6ecb00b63a75b01f6eb82669f8e
Sergei Makarov <sergei.makarov997@gmail.com>
2022-08-24 16:37:21 +0200

    adding POJOs. Closes #2

---
+++ src/main/java/com/example/sfgpetclinic/SFGPetClinic/model/Owner.java
@@ -0,0 +1,10 @@
+package com.example.sfgpetclinic.SFGPetClinic.model;
+
+public class Owner extends Person {
+
+	public Owner(String firstName, String lastName) {
+		super(firstName, lastName);
+		// TODO Auto-generated constructor stub
+	}
+
+}
