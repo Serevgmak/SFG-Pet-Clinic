0dede78ce5242be94b89e94dad07fcd439fee2af
Sergei Makarov <sergei.makarov997@gmail.com>
2022-08-29 10:18:01 +0200

    Vet Controller Updated. Closes #15

--- pet-clinic-web/src/main/java/com/example/sfgpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/example/sfgpetclinic/controllers/VetController.java
@@ -1,13 +1,25 @@
 package com.example.sfgpetclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.sfgpetclinic.services.VetService;
 
 @Controller
 public class VetController {
 
+	private final VetService vetService;
+
+	public VetController(VetService vetService) {
+		super();
+		this.vetService = vetService;
+	}
+
 	@RequestMapping({ "/vets", "/vets/", "/vets/index", "/vets/index.html" })
-	public String listVets() {
+	public String listVets(Model model) {
+
+		model.addAttribute("vets", vetService.findAll());
 
 		return "vets/index";
 	}
