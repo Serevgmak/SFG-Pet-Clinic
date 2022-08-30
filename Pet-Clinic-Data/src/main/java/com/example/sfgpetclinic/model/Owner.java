49e859f6dd6bc16f2592b123e119581d316e86e2
Sergei Makarov <sergei.makarov997@gmail.com>
2022-08-30 16:46:29 +0200

    Added Properties to Owner. Closes #28

--- Pet-Clinic-Data/src/main/java/com/example/sfgpetclinic/model/Owner.java
+++ Pet-Clinic-Data/src/main/java/com/example/sfgpetclinic/model/Owner.java
@@ -4,6 +4,10 @@ import java.util.Set;
 
 public class Owner extends Person {
 
+	private String address;
+	private String city;
+	private String telephone;
+
 	private Set<Pet> pets;
 
 	public Owner(String firstName, String lastName) {
@@ -15,8 +19,32 @@ public class Owner extends Person {
 		// TODO Auto-generated constructor stub
 	}
 
+	public String getAddress() {
+		return address;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public String getTelephone() {
+		return telephone;
+	}
+
 	public Set<Pet> getPets() {
 		return pets;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public void setTelephone(String telephone) {
+		this.telephone = telephone;
 	}
 
 	public void setPets(Set<Pet> pets) {
