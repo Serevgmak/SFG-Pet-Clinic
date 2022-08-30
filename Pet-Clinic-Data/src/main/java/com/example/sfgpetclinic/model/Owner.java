f241a22ff4942fc760265acdfcc347ca0653c8a0
Sergei Makarov <sergei.makarov997@gmail.com>
2022-08-30 16:38:41 +0200

    Added Visits, updated Owner with Pet Relationship. Closes #26

--- Pet-Clinic-Data/src/main/java/com/example/sfgpetclinic/model/Owner.java
+++ Pet-Clinic-Data/src/main/java/com/example/sfgpetclinic/model/Owner.java
@@ -1,6 +1,10 @@
 package com.example.sfgpetclinic.model;
 
+import java.util.Set;
+
 public class Owner extends Person {
+
+	private Set<Pet> pets;
 
 	public Owner(String firstName, String lastName) {
 		super(firstName, lastName);
@@ -9,6 +13,14 @@ public class Owner extends Person {
 
 	public Owner() {
 		// TODO Auto-generated constructor stub
+	}
+
+	public Set<Pet> getPets() {
+		return pets;
+	}
+
+	public void setPets(Set<Pet> pets) {
+		this.pets = pets;
 	}
 
 }
