23b67eca5c24d3731f5efc1b04faf8aefe400469
Sergei Makarov <sergei.makarov997@gmail.com>
2022-08-30 16:58:57 +0200

    Added PetTypeService. Closes #29

---
+++ Pet-Clinic-Data/src/main/java/com/example/sfgpetclinic/services/map/PetTypeServiceMap.java
@@ -0,0 +1,43 @@
+package com.example.sfgpetclinic.services.map;
+
+import java.util.Set;
+
+import org.springframework.stereotype.Service;
+
+import com.example.sfgpetclinic.model.PetType;
+import com.example.sfgpetclinic.services.PetTypeService;
+
+@Service
+public class PetTypeServiceMap extends AbstractMapService<PetType, Long> implements PetTypeService {
+
+	@Override
+	public Set<PetType> findAll() {
+		// TODO Auto-generated method stub
+		return super.findAll();
+	}
+
+	@Override
+	public PetType findById(Long id) {
+		// TODO Auto-generated method stub
+		return super.findById(id);
+	}
+
+	@Override
+	public PetType save(PetType object) {
+		// TODO Auto-generated method stub
+		return super.save(object);
+	}
+
+	@Override
+	public void deleteById(Long id) {
+		// TODO Auto-generated method stub
+		super.deleteById(id);
+	}
+
+	@Override
+	public void delete(PetType object) {
+		// TODO Auto-generated method stub
+		super.delete(object);
+	}
+
+}
