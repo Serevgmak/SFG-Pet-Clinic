487b891f01351a0b2b37cfdf793859aba4ec21c1
Sergei Makarov <sergei.makarov997@gmail.com>
2022-09-05 16:59:22 +0200

    Closes #46

---
+++ Pet-Clinic-Data/src/main/java/com/example/sfgpetclinic/services/map/VisitMapService.java
@@ -0,0 +1,44 @@
+package com.example.sfgpetclinic.services.map;
+
+import java.util.Set;
+
+import com.example.sfgpetclinic.model.Visit;
+import com.example.sfgpetclinic.services.VisitService;
+
+public class VisitMapService extends AbstractMapService<Visit, Long> implements VisitService {
+
+	@Override
+	public Set<Visit> findAll() {
+		// TODO Auto-generated method stub
+		return super.findAll();
+	}
+
+	@Override
+	public Visit findById(Long id) {
+		// TODO Auto-generated method stub
+		return super.findById(id);
+	}
+
+	@Override
+	public Visit save(Visit visit) {
+
+		if (visit.getPet() == null || visit.getPet().getOwner() == null || visit.getPet().getId() == null
+				|| visit.getPet().getOwner().getId() == null) {
+			throw new RuntimeException("Invalid visit");
+		}
+		return super.save(visit);
+	}
+
+	@Override
+	public void deleteById(Long id) {
+		// TODO Auto-generated method stub
+		super.deleteById(id);
+	}
+
+	@Override
+	public void delete(Visit object) {
+		// TODO Auto-generated method stub
+		super.delete(object);
+	}
+
+}
